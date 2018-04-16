6051ba86f9641a6af1ec4a4006c7c9d13bdf6bff
李冠军 <lgj@moojnn.com>
2018-04-16 15:03:55 +0800

    增加回调

--- bin/mongod.js
+++ bin/mongod.js
@@ -16,25 +16,23 @@ dbFactory.saveInDB = function(param, callback) {
             if (callback) {
                 callback();
             }
-            console.log(param.date);
             db.close();
         });
     });
 };
 
-dbFactory.findAll = function(){
+dbFactory.findAll = function(callback){
     mongoClient.connect(url, function (err, db) {
         if (err) throw err;
         var dbo = db.db("weibo");
         var collection = dbo.collection("财上海");
 
-        collection.find().toArray(function (err, docs) {
+        collection.find().sort({date: -1}).toArray(function (err, docs) {
             dbFactory.data = docs;
+            callback();
             db.close();
         });
     });
 };
-
-dbFactory.findAll();
 
 module.exports = dbFactory;
