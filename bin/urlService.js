2c15ed881cbb82be8d226731c801617a660f85b3
李冠军 <lgj@moojnn.com>
2018-04-17 15:01:35 +0800

    修改README.md

--- bin/urlService.js
+++ bin/urlService.js
@@ -1,6 +1,9 @@
 var urls = [];
 
-var pageNo = 765;
+//若抓取超过50页，微博服务器缓存溢出，会暂时拒绝服务
+//建议每次抓取不超过50页
+var pageNo = [230, 260];
+
 var str1 = 'https://weibo.com/u/1565668374?pids=Pl_Official_MyProfileFeed__21&is_search=0&visible=0&is_all=1&is_tag=0&profile_ftype=1&page=',
     str2 = '&ajaxpagelet=1&ajaxpagelet_v6=1&__ref=%2Fu%2F1565668374%3Fis_search%3D0%26visible%3D0%26is_all%3D1%26is_tag%3D0%26profile_ftype%3D1%26page%3D2%23feedtop&_t=FM_152358484928032';
 var _str1 = 'https://weibo.com/p/aj/v6/mblog/mbloglist?ajwvr=6&domain=100505&topnav=1&wvr=6&topsug=1&is_all=1&pagebar=',
@@ -10,8 +13,7 @@ var _str1 = 'https://weibo.com/p/aj/v6/mblog/mbloglist?ajwvr=6&domain=100505&top
 function getURL() {
     var i, j;
 
-    //若抓取超过50页，微博服务器缓存溢出，会暂时拒绝服务
-    for (i = 565; i < 730; i++) {
+    for (i = pageNo[0]; i < pageNo[1]; i++) {
         urls.push({type: 0, url: str1 + i + str2});
         for (j = 0; j < 2; j++) {
             urls.push({type: 1, url: _str1 + j + _str2 + i + _str3 + new Date().getTime()})
